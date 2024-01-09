013276ed0f0aaad8d8dbd69eb0110cc2d7e35513
FadiBadarni <fadybd1@gmail.com>
2024-01-09 22:43:12 +0200

    Remove unused CSS, test, logo, and web vitals files, and update store import path.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './store/store';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const container = document.getElementById('root') as HTMLElement;
