741939eeac4a0586b1c854c9d2674d7ea25163d0
FadiBadarni <fadybd1@gmail.com>
2024-01-15 13:26:03 +0200

    Refactor useAuth.ts: Remove unused imports and token expiration check

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,21 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import {
-  setAccessToken,
-  clearAccessToken,
-  setAuthError,
-} from 'features/auth/authReducer';
-import { jwtDecode } from 'jwt-decode';
+import { setAccessToken } from 'features/auth/authReducer';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 
-interface DecodedToken {
-  exp: number;
-}
-
 export const useCustomAuth = () => {
   const { getAccessTokenSilently } = useAuth0();
   const dispatch = useDispatch();
+
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
 
   useEffect(() => {
@@ -27,24 +19,12 @@ export const useCustomAuth = () => {
           },
         });
         dispatch(setAccessToken(token));
-      } catch (error: any) {
-        console.error('Error fetching access token', error);
-        dispatch(clearAccessToken());
-        dispatch(setAuthError('Failed to fetch access token'));
-      }
-    };
-
-    const isTokenExpired = (token: string): boolean => {
-      try {
-        const decoded: DecodedToken = jwtDecode(token);
-        return decoded.exp < Date.now() / 1000;
       } catch (error) {
-        console.error('Error decoding access token', error);
-        return true;
+        console.error('Error fetching access token', error);
       }
     };
 
-    if (!accessToken || isTokenExpired(accessToken)) {
+    if (!accessToken) {
       fetchAccessToken();
     }
   }, [getAccessTokenSilently, dispatch, accessToken]);
